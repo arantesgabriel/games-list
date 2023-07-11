71e61b984c7c1736e22f966a599c419aa5bf4f72
Gabriel Arantes <gabrielpacontato@gmail.com>
2023-07-11 00:10:28 -0300

    Criação do projeto, junto a sua entidade principal.

---
+++ src/main/java/com/devsuperior/gameslist/entities/Game.java
@@ -0,0 +1,111 @@
+package com.devsuperior.gameslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "games")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String titulo;
+    private Double avaliacao;
+    private Integer ano;
+    private String genero;
+    private String plataformas;
+    private String imgUrl;
+    private String descricaoCurta;
+    private String descricaoLonga;
+
+    public Game() {
+
+    }
+
+    public Game(Long codigo, String titulo, Double avaliacao, Integer ano, String genero, String plataformas, String imgUrl, String descricaoCurta, String descricaoLonga) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.avaliacao = avaliacao;
+        this.ano = ano;
+        this.genero = genero;
+        this.plataformas = plataformas;
+        this.imgUrl = imgUrl;
+        this.descricaoCurta = descricaoCurta;
+        this.descricaoLonga = descricaoLonga;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getPlataformas() {
+        return plataformas;
+    }
+
+    public void setPlataformas(String plataformas) {
+        this.plataformas = plataformas;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getDescricaoCurta() {
+        return descricaoCurta;
+    }
+
+    public void setDescricaoCurta(String descricaoCurta) {
+        this.descricaoCurta = descricaoCurta;
+    }
+
+    public String getDescricaoLonga() {
+        return descricaoLonga;
+    }
+
+    public void setDescricaoLonga(String descricaoLonga) {
+        this.descricaoLonga = descricaoLonga;
+    }
+
+}
